752999f2abbe40f9cd6336e2fe166a9f1152bdea
jie.luo <190642964@qq.com>
2018-06-07 14:04:16 +0800

    配置home-platform注册中心(eureka)

--- home-registry/src/main/java/com/home/registry/RegistryApplication.java
+++ home-registry/src/main/java/com/home/registry/RegistryApplication.java
@@ -1,14 +1,20 @@
 package com.home.registry;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
- * ${DESCRIPTION}
+ * 注册中心
  *
  * @author roger
  * @email 190642964@qq.com
  * @create 2018-06-06 15:48
  **/
+@SpringBootApplication
+@EnableEurekaServer
 public class RegistryApplication {
     public static void main(String[] args){
-
+        SpringApplication.run(RegistryApplication.class,args);
     }
 }
